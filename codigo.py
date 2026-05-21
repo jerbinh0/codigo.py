138a4589de5f5c5296ec6ed714b50f3488dd46de
jerbinh0 <jerbinho.366@gmail.com>
2026-05-21 09:00:01 -0300

    Implement random code generator in codigo.py
    Add a function to generate a random code of specified length.

---
+++ codigo.py
@@ -0,0 +1,10 @@
+# codigo.py
+import random
+import string
+
+def gerar_codigo(tamanho=10):
+    caracteres = string.ascii_letters + string.digits
+    codigo = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return codigo
+
+print("Código gerado:", gerar_codigo())
